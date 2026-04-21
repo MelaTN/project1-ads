9ac68c9b5e736b90fa9954b130799bf7cbf5933d
Melanie <melni24@student.sdu.dk>
2026-04-21 22:33:01 +0200

    skelet

---
+++ DictBinTree.py
@@ -0,0 +1,20 @@
+class DictBinTree:
+    def __init__(self, root: BinNode = None):
+        self.root = root
+
+    def search(self, k): 
+        pass
+
+    def insert(self, k):
+        pass
+
+    def orderedTraversal(self):       
+        pass
+
+
+class BinNode:
+    def __init__(self, left: BinNode = None, right: BinNode = None):
+        self.left = left   
+        self.right = right     
+
+    
