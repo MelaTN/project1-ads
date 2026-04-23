843c954351b6cb636c78af740e4ecd8be78e8be1
Melanie <melni24@student.sdu.dk>
2026-04-23 21:03:52 +0200

    Implementeringer v1

--- DictBinTree.py
+++ DictBinTree.py
@@ -2,19 +2,66 @@ class DictBinTree:
     def __init__(self, root: BinNode = None):
         self.root = root
 
-    def search(self, k): 
-        pass
+
+    ## Tree search ##
+
+    def search(self, k):
+        "Returns a pointer to a node with key k if such a node exists."
+        return self._search(self.root, k)
+
+    def _search(self, x, k): 
+        "Performs recursive step in tree search starting at node x."
+        if x == None or k == x.key:
+            return x
+        if k < x.key:
+            return self.search(x.left,k)
+        else:
+            return self.search(x.right,k)
+        
+    ## Insert ##
 
     def insert(self, k):
-        pass
+        "Inserts node with key key into the binary search tree."
+        x = self.root
+        y = None 
+        while x != None:
+            y = x
+        if k < x.key:
+            x = x.left
+        else:
+            x = x.right
+        
+        # Now, x is poiting to a None and y is poiting to the 
+        # parent of this leaf. In order to replace the value None in the
+        # tree structure we need to change that child of y.
 
-    def orderedTraversal(self):       
-        pass
+        if y == None:
+            self.root = BinNode(k) # Tree was empty
+        elif k < y.key:
+            y.left = BinNode(k)
+        else:
+            y.right = BinNode(k)
+        
+    ## Inorder tree walk ##
+
+    def orderedTraversal(self, k):
+        "Returns a sorted list of the keys in the binary search tree."
+        return self._orderedTraversal(self.root, [], k)
+
+    def _orderedTraversal(self, x, l:list, k):
+        ""       
+        l.append(self.orderedTraversal(x.left, k))
+        l.append(x.key)
+        l.append(self.orderedTraversal(x.right, k))
+
+        return l
 
 
 class BinNode:
-    def __init__(self, left: BinNode = None, right: BinNode = None):
+    def __init__(self, key: float, left: BinNode = None, right: BinNode = None):
+        self.key = key
         self.left = left   
         self.right = right     
+
 
     
